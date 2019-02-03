fedc437acdc262907155c338ca144cfdcdf3d60a
thekthuser <thekthuser@thekthuser.com>
2019-02-03 16:32:19 -0500

    populate Indices in /populate/sql

--- routes/populate.js
+++ routes/populate.js
@@ -12,12 +12,14 @@ router.get('/sql', function(req, res) {
     if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
     console.log('Connected to the in-memory SQlite database.');
   });
+
   db.serialize( () => {
     db.run('DROP TABLE IF EXISTS Users;', (err)=> {
       if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
       console.log('The Users table was dropped.');
     });
-    db.run('CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, is_advanced INTEGER);', (err)=> {
+    db.run('CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+      username TEXT UNIQUE, is_advanced INTEGER);', (err)=> {
       if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
       console.log('The Users table was created.');
     });
@@ -37,6 +39,38 @@ router.get('/sql', function(req, res) {
       if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
       console.log('A row has been inserted.');
     });
+
+
+    db.run('DROP TABLE IF EXISTS Indices;', (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('The Indices table was dropped.');
+    });
+    db.run('CREATE TABLE IF NOT EXISTS Indices (id INTEGER PRIMARY KEY AUTOINCREMENT, \
+      name TEXT UNIQUE, owner TEXT, description TEXT);', (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('The Indices table was created.');
+    });
+    db.run('INSERT INTO Indices (name, owner, description) VALUES(?, ?, ?);', 
+      ['foo_index', 'foo', 'People whose first names start with "f"'], (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('A row has been inserted.');
+    });
+    db.run('INSERT INTO Indices (name, owner, description) VALUES(?, ?, ?);', 
+      ['bar_index', 'bar', 'Real people.'], (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('A row has been inserted.');
+    });
+    db.run('INSERT INTO Indices (name, owner, description) VALUES(?, ?, ?);', 
+      ['baz_index', 'baz', 'Fictional people.'], (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('A row has been inserted.');
+    });
+    db.run('INSERT INTO Indices (name, owner, description) VALUES(?, ?, ?);', 
+      ['buzz_index', 'buzz', 'All people.'], (err)=> {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      console.log('A row has been inserted.');
+    });
+
     db.close();
   });
   res.send("Populate SQL");
