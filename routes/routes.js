4feff6934665218b1be24155513f3e118bfa7bd3
thekthuser <thekthuser@thekthuser.com>
2019-02-03 18:00:49 -0500

    /users/:username working. needs to check for bad username

--- routes/routes.js
+++ routes/routes.js
@@ -18,6 +18,28 @@ router.get('/users', function(req, res) {
     });
     db.close();
   });
-})
+});
+
+router.get('/users/:username', function(req, res) {
+  username = req.params.username;
+  let db = new sqlite3.Database('./db/sqlite.db', (err) => {
+    if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+  db.serialize( () => {
+    db.all('SELECT Indices.id, Indices.name, Indices.owner, Users.is_advanced FROM Indices LEFT JOIN Users ON Indices.owner = Users.username;', [], (err, rows) => {
+      if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
+      let indices = [];
+      rows.forEach((row) => {
+        console.log('owner: ' + row.owner + ' username: ' + username);
+        if (!row.is_advanced || (row.is_advanced && row.owner == username)) {
+          indices.push(row);
+        }
+      });
+      res.send(indices);
+    });
+    db.close();
+  });
+});
 
 module.exports = router;
