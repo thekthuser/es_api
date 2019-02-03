0ed7c4735332f45344093e7553fbd0fdba4fca14
thekthuser <thekthuser@thekthuser.com>
2019-02-03 16:13:27 -0500

    change var to let

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-var routes = require('./routes/routes');
-var populate_routes = require('./routes/populate');
+let routes = require('./routes/routes');
+let populate_routes = require('./routes/populate');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Listening on port ${port}!`))
 app.use('/', routes);
 app.use('/populate', populate_routes);
 
