ebe30baa45a41729d18bceb4957adf0c16532098
thekthuser <thekthuser@thekthuser.com>
2019-02-05 14:08:29 -0500

    /_search/:index check user has access to index

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
+const elasticsearch = require('elasticsearch');
+const fetch = require('node-fetch');
 
 router.get('/', function(req, res) {
   res.send('index');
@@ -21,7 +23,7 @@ router.get('/users', function(req, res) {
 });
 
 router.get('/users/:username', function(req, res) {
-  let username = req.params.username;
+  let username = req.params.username.toLowerCase();
   let db = new sqlite3.Database('./db/sqlite.db', (err) => {
     if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
     console.log('Connected to the in-memory SQlite database.');
@@ -62,18 +64,54 @@ router.get('/users/:username', function(req, res) {
   });
 });
 
-router.get('/_search/:index', function(req, res) {
-  let index = req.params.index;
+router.get('/_search/:index', function(req, res, next) {
+  let client = elasticsearch.Client({
+    host: 'localhost:9200'
+  });
+  let index = req.params.index.toLowerCase();
   let db = new sqlite3.Database('./db/sqlite.db', (err) => {
     if (err) { console.error(err.message); res.status(500).send('500 Internal Server Error'); }
     console.log('Connected to the in-memory SQlite database.');
   });
   db.serialize( () => {
 
-    // function return indices has access too, check in there
-    let index_promise = new Promise(function(resolve, reject) {
-      Response.fetch('/');
+    //assume user 'foo'
+    let user = 'foo';
+    let index = req.params.index.toLowerCase();
+    //assume http://localhost:3000
+    let indices = fetch('http://localhost:3000/users/' + user);
+    indices.then(function (resp) {
+      resp.json().then(function(resp2) {
+        let allowed = false;
+        resp2.forEach(function(ind) {
+          if (index == ind.name) {
+            allowed = true;
+          }
+        });
+        if (!allowed) {
+          // this is broken, i think it's because the fetch above sends a response
+          console.error('Unauthorized index.'); res.status(401).send('401 Unauthorized');
+          //res.send('401 Unauthorized.');
+          //next('401 Unauthorized.');
+        }
+      });
     });
+    /*
+    let test_search = client.search({
+      index: 'foo_index',
+      q: 'first_name:fred'
+    });
+    test_search.then(function(resp) {
+      console.log('AAAAAAAAAAAAAAAA');
+      console.log(resp);
+      console.log('BBBBBBBBBBBBBBB');
+      console.log(resp.hits);
+      console.log('CCCCCCCCCCCCC');
+      console.log(resp.hits.hits[0]);
+      console.log('DDDDDDDDDDDDDD');
+      console.log(resp.hits.hits[0]._source);
+    });
+    */
 
 
     res.send('_search');
